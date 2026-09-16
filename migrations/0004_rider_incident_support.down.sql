b2c7d41bf8fe007fece63b08ea8eb20cad5519ba
NurionHoldingsInSuk <238840779+NurionHoldings@users.noreply.github.com>
2026-09-16 15:58:53 +0900

    fix: make incident rollback safe before initial schema

--- migrations/0004_rider_incident_support.down.sql
+++ migrations/0004_rider_incident_support.down.sql
@@ -2,5 +2,9 @@ BEGIN;
 DROP TABLE IF EXISTS rider_incident_audit;
 DROP TABLE IF EXISTS rider_incident_cases;
 DROP TABLE IF EXISTS rider_coverage_snapshots;
-DELETE FROM schema_migrations WHERE version=4;
+DO $down$ BEGIN
+ IF to_regclass('schema_migrations') IS NOT NULL THEN
+  EXECUTE 'DELETE FROM schema_migrations WHERE version=4';
+ END IF;
+END $down$;
 COMMIT;
